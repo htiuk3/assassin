0b0ba45adb959f7416cf8eb4151ab60b95c0a6e4
unknown <williamdat10@gmail.com>
2020-09-05 06:52:05 +0700

    Modify php to html

---
+++ JS_Study/main.js
@@ -0,0 +1,12 @@
+// //Kiểu chuỗi
+var myStr = "Hoc JS cho nguoi moi bat dau"
+// 1. 
+// 2.
+// 3. Cut string
+// 4. Convert to Upper case
+// 5. Conver to Lower case
+// 6. Trim
+console.log(myStr.trim())
+// 7. Replace
+// 8. Split
+// 9. Get a charater by index
