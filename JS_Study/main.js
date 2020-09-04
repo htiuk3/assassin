12e7c2446e69b0207b796ec791c87305d0114bfd
unknown <williamdat10@gmail.com>
2020-09-05 06:58:07 +0700

    update main.js

--- JS_Study/main.js
+++ JS_Study/main.js
@@ -6,7 +6,13 @@ var myStr = "Hoc JS cho nguoi moi bat dau"
 // 4. Convert to Upper case
 // 5. Conver to Lower case
 // 6. Trim
-console.log(myStr.trim())
+console.log(myStr.trim()) // cat bo khoang trang thua
 // 7. Replace
 // 8. Split
+var languages = 'Javascript, PHP, Ruby';
+console.log(languages.split(', ')); //cat chuoi theo mot diem chung
+languages = 'Javascript';
+console.log(languages.split(''));
 // 9. Get a charater by index
+const myStr2 = 'Dat Map';
+console.log(myStr2.charAt(0)) // Lay ki tu o vi tri truyen vao
