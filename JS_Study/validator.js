7eeb4dc412c95d5c855752e975fe1c17b056f639
unknown <williamdat10@gmail.com>
2020-09-22 21:32:37 +0700

    update files

--- JS_Study/validator.js
+++ JS_Study/validator.js
@@ -1,31 +1,121 @@
 
 
+function Validator (formSelector) {
+    var getParent = function (element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    }
+    var formRules = {};
+    var validatorRules = {
+        required: function(value) {
+            return value ? undefined : 'Vui lòng nhập trường này';
+        },
+        email: function(value) {
+            var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regex.test(value) ? undefined : 'Trường này phải là email';
+        },
+        min: function (min) {
+            return function(value) {
+                return value.length >= min  ? undefined : `Bạn cần nhập ít nhất ${min} ký tự`;
+            }
+        },
+        max: function (max) {
+            return function (value) {
+                return value.length <= max  ? undefined : `Bạn cần nhập tối đa ${max} ký tự`;
+            }
+        }
+    }
 
-
-function Validator(options) {
-    
-    var formElement = document.querySelector(options.form);
-    
+    const formElement = document.querySelector(formSelector);
     if (formElement) {
-        options.rules.forEach(function(rule) {
-            console.log(rule.selector);
-        });
-    }
-}
+        var inputs = formElement.querySelectorAll("[name][rules]");
+        
+        for (var input of inputs) {
+            var rules = input.getAttribute('rules').split('|');
+            for (var rule of rules) {
+                var ruleInfo;
+                var isRuleHasValue = rule.includes(':');
+                if (rule.includes(':')) {
+                    ruleInfo = rule.split(':');
+                    rule = ruleInfo[0];
+                }
+                var ruleFunc = validatorRules[rule];
 
-Validator.isRequired = function (selector) {
-    return {
-        selector: selector,
-        test: function () {
+                if (isRuleHasValue) {
+                    ruleFunc = ruleFunc(ruleInfo[1]);
+                }
+
+                if (Array.isArray(formRules[input.name])) {
+                    formRules[input.name].push(ruleFunc);
+                } else {
+                    formRules[input.name] = [ruleFunc];
+                }
+
+            }
+            // Lắng nghe sự kiện để validate
+            input.onblur = handleValidate;
+            input.oninput = handleClearError;
+        }
+        
+        function handleValidate(event) {
+            var rules = formRules[event.target.name];
+            var errorMessage; 
+            
+            for (var rule of rules) {
+                errorMessage = rule(event.target.value);
+                if (errorMessage) break;
+            }
+            
+            if (errorMessage) {
+                var formGroup = getParent(event.target, '.form-group');
+                if (formGroup) {
+                    formGroup.classList.add('invalid');
+                    var formMessage = formGroup.querySelector('.form-message');
+                    if (formMessage) {
+                        formMessage.innerText = errorMessage;
+                    }
+                }
+            }
+            return !errorMessage;
+        }
+
+        function handleClearError(event) {
+            var formGroup = getParent(event.target, '.form-group');
+            if (formGroup.classList.contains('invalid')) {
+                formGroup.classList.remove('invalid');
+            }
+            var formMessage = formGroup.querySelector('.form-message');
+            if (formMessage) {
+                formMessage.innerText = '';
+            }
+        }
+
+    }
+
+    //Xử lý hành vi submit form
+    formElement.onsubmit = function (event) {
+        event.preventDefault();
+        var inputs = formElement.querySelectorAll("[name][rules]");
+        var isValid = true;
+        for (var input of inputs) {
+            if (!handleValidate({ target: input })) {
+                isValid = false;
+            };
 
         }
-    }
-}
-Validator.isEmail = function(selector) {
-    return {
-        selector: selector,
-        test: function () {
 
+        // Khi không có lỗi thì submit form
+        if (isValid) {
+            if (typeof options.onSubmit === 'function') {
+                options.onSubmit();
+            } else {
+                formElement.submit();
+            }
         }
     }
+
 }
